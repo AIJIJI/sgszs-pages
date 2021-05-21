02c30641a4d1eac55154ca3053b87f31819e5e46
钟雪松 <xuesong.zhong@uwa4d.com>
2021-05-21 10:25:34 +0800

    mobile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <button className="btn-skill" data-poolname="Chupai" bindtap="tapxushaoActive">
+          <img
+            className="icon"
+            src="style/images/xushao.jpg"
+            mode="aspectFit"
+            alt="出牌阶段"
+          />
+          <span className="skill-display">出牌阶段</span>
+        </button>
+      </div>
+    );
+  }
 }
-
 export default App;
